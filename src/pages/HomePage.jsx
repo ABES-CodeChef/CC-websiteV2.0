a45202b43dd484659eaf24ad9da9007bbf656388
PriyatoshKumarShahi <priytoshshahi90@gmail.com>
2025-11-07 14:47:17 +0530

    Adding Landing Section and fixing app.jsx issues

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,11 @@
+import Landing from "../components/Landing";
+import Offering from "../components/Offering";
+
+export default function HomePage() {
+  return (
+    <div className="w-full">
+      <Landing />
+      <Offering/>
+    </div>
+  );
+}
